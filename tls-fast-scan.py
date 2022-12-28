57f299d56dde1b76d3fab0f164c4c921fb2b8788
Bart van der Braak <bart.vanderbraak@wearetriple.com>
2022-12-28 01:45:55 +0100

    initial idea of the class

--- tls-fast-scan.py
+++ tls-fast-scan.py
@@ -1,8 +1,8 @@
-import multiprocessing
-import subprocess
+import subprocess as sp
 from xml.etree import ElementTree
 import concurrent.futures
 import os
+import functools
 
 hostname_list = [
     "www.google.com",
@@ -10,75 +10,36 @@ hostname_list = [
     "www.facebook.com",
     "www.twitter.com",
     "www.instagram.com",
-    "www.wikipedia.org",
-    "www.yahoo.com",
-    "www.whatsapp.com",
-    "www.amazon.com",
-    "www.live.com",
-    "www.netflix.com",
-    "www.tiktok.com",
-    "www.reddit.com",
-    "www.office.com",
-    "www.linkedin.com",
-    "www.samsung.com",
-    "www.vk.com",
-    "www.turbopages.org",
-    "www.naver.com",
-    "www.bing.com",
-    "www.discord.com",
-    "www.twitch.tv",
-    "www.pinterest.com",
-    "www.zoom.us",
-    "www.weather.com",
-    "www.qq.com",
-    "www.microsoft.com",
-    "www.msn.com",
-    "www.globo.com",
-    "www.duckduckgo.com",
-    "www.roblox.com",
-    "www.quora.com",
-    "www.ebay.com",
 ]
 
+def check_nmap():
+    check_version = sp.run(['nmap', '--version'], stderr=sp.PIPE, stdout=sp.PIPE)
+    return check_version.returncode == 0
 
-def check_if_nmap_is_installed():
-    result = subprocess.run(['nmap', '--version'], stderr=subprocess.PIPE)
-    if result.returncode == 0:
-        return True
-    else:
-        return False
+def xml_to_tls_versions(xml_data):
+    root = ElementTree.fromstring(xml_data)
+    return [table.attrib['key'] for table in root.findall(".//script[@id='ssl-enum-ciphers']/table")]
 
+class Scanner:
+    # Constructor
+    def __init__(self, hostnames, workers=os.cpu_count()):
+        self.nmap = None
+        self.results = {}
+        self.hostnames = hostnames
+        self.workers = workers
 
-def get_nmap_xml(hostname, xml_data):
-    if hostname not in xml_data:
-        xml_data[hostname] = subprocess.run(
-            ['nmap', '--script', 'ssl-enum-ciphers', '-p', '443', '-oX', '-', hostname],
-            stdout=subprocess.PIPE).stdout
-    return xml_data[hostname]
+    def tls_versions(self, hostname, port=443):
+        self.nmap = f'nmap -sV --script ssl-enum-ciphers -p {port} -oX - {hostname}'
+        self.results[hostname] = xml_to_tls_versions(sp.run(self.nmap, stdout=sp.PIPE).stdout)
+        print('hi')
 
-
-def get_nmap_tls_version(hostname, xml_data):
-    xml_str = get_nmap_xml(hostname, xml_data)
-    root = ElementTree.fromstring(xml_str)
-    unique_tls_versions = [table.attrib['key'] for table in root.findall(".//script[@id='ssl-enum-ciphers']/table")]
-    return unique_tls_versions
-
-
-def main():
-    if check_if_nmap_is_installed():
-        manager = multiprocessing.Manager()
-        xml_data = manager.dict()
-
-        num_workers = os.cpu_count()
-        with concurrent.futures.ProcessPoolExecutor(num_workers) as executor:
-            results = list(executor.map(get_nmap_tls_version, hostname_list, [xml_data]*len(hostname_list)))
-
-        for hostname, tls_versions in zip(hostname_list, results):
-            print(f'TLS versions available on {hostname}: {tls_versions}')
-
-    else:
-        print('nmap is not installed on this system')
+    def process_hostnames(self):
+        print('hi')
+        with concurrent.futures.ProcessPoolExecutor(max_workers=self.workers) as executor:
+            executor.map(self.tls_versions, self.hostnames)
 
 
 if __name__ == '__main__':
-    main()
+    scan = Scanner(hostnames=hostname_list)
+    scan.process_hostnames()
+    print(scan.results)
